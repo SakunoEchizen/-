d02efcf0378dbb13b15d5d8b185d0af7a39ed86e
汉祥 刘 <liuhanxiang520@outlook.com>
2021-07-14 17:42:18 +0800

    Bubble sort

--- 冒泡排序/冒泡排序.cpp
+++ 冒泡排序/冒泡排序.cpp
@@ -4,10 +4,46 @@
 #include <iostream>
 using namespace std;
 
+int *Bubble_Sort(int *a)
+{
+    int temp;
+
+    int length = sizeof( a ) /sizeof(a[0]);
+    cout << "将一个函数A里面定义的数组当参数传给函数B时，在B里面计算：length of sequence :" << length << endl;
+    //当将数组作为实参传递到另一个函数中时, 另一个函数的形参相当于一个指针变量, 因为将数组的名作为实参时, 就是将数字的首地址作为实参
+    length = 9;
+    for (int i = 0; i < length-1; i++)
+    {
+        for (int j=0; j < length -1-i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    return a;
+    //想要返回一个数组，那么函数必须是指针函数，且指着指针函数的形参也是指针才可以
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    int a[9] = { 6,3,2,6,3,1,0,4,5};
+    int *b;
+    int length = sizeof(a) / sizeof(int);
+
+    cout << "在定义数组的函数A计算:length of sequence :" << length << endl;
+    b=Bubble_Sort(a);
+    cout << "Bubble_Sort(冒泡排序）:" << endl;
+    for (int i = 0; i < 9; i++)
+    {
+        cout << b[i]<<" ";
+    }
+    return 0;
 }
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
